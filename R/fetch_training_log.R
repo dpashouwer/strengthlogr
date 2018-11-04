fcf698ad142eef99a1fdbdca8e67f57c60aa5328
Dustin Pashouwer <dustin.pashouwer@tntp.org>
2018-11-04 10:28:19 -0700

    Updated fetch_strength_log

--- R/fetch_training_log.R
+++ R/fetch_training_log.R
@@ -8,14 +8,14 @@
 #'
 #' @examples
 fetch_training_log <- function(key = "1UGyjRtesbrW7URbJZOr2j-3J2VL894SxigRlHquGYNg", worksheet = "StrengthLog"){
-  gs_key(key) %>%
-    gs_read(ws = worksheet) %>%
-    clean_names() %>%
-    mutate(date = mdy(date),
+  googlesheets::gs_key(key) %>%
+    googlesheets::gs_read(ws = worksheet) %>%
+    janitor::clean_names() %>%
+    dplyr::mutate(date = lubridate::mdy(date),
            reps = reps %>% as.numeric(),
            weight = weight %>% as.numeric()) %>%
 
-    filter(!is.na(reps)) %>%
-    select(date, exercise, type, reps, weight, e1rm) %>%
-    mutate(week = floor_date(date, unit = "weeks"))
+    dplyr::filter(!is.na(reps)) %>%
+    dplyr::select(date, exercise, type, reps, weight, e1rm) %>%
+    dplyr::mutate(week = lubridate::floor_date(date, unit = "weeks"))
 }
