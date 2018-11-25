5b23dfbe3d5559d8eb97c0942b31def12f146ca2
Dustin Pashouwer <dustin.pashouwer@tntp.org>
2018-11-24 17:39:32 -0700

    Documented subfunctions add_exercise(), new_workout()

--- R/build_microcycle.R
+++ R/build_microcycle.R
@@ -20,12 +20,33 @@ build_microcycle <- function(){
   microcycle
 }
 
+#' Add workout to a microcycle
+#'
+#' @param microcycle
+#'
+#' @return
+#' @export
+#'
+#' @examples
 new_workout <- function(microcycle){
   current_day = max(microcycle$day)
   microcycle %>%
     dplyr::add_row(day = current_day + 1)
 }
 
+#' Add Exercist to a workout
+#'
+#' @param microcycle
+#' @param exercise
+#' @param sets
+#' @param target_reps
+#' @param target_rpe
+#' @param target_pct
+#'
+#' @return
+#' @export
+#'
+#' @examples
 add_exercise <- function(microcycle, exercise, sets, target_reps = NA_character_, target_rpe = NA_integer_, target_pct = NA_integer_){
   microcycle %>%
     dplyr::add_row(exercise = exercise, sets = 1:sets, target_reps = target_reps, target_rpe = target_rpe, target_pct = target_pct) %>%
