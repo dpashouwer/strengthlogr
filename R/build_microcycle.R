654b15dbeca077027f98fdca6b10cf656fa1d942
Dustin Pashouwer <dustin.pashouwer@tntp.org>
2018-11-24 17:34:11 -0700

    Added build_microcycle()

---
+++ R/build_microcycle.R
@@ -0,0 +1,34 @@
+
+
+#' Build Microcycle
+#'
+#' @return
+#' @export
+#'
+#' @examples
+build_microcycle <- function(){
+  microcycle <- tibble::tibble(week = numeric(),
+                               day = numeric(),
+                               exercise = character(),
+                               sets = numeric(),
+                               type = character(),
+                               target_reps = character(),
+                               target_rpe = numeric(),
+                               target_pct = numeric()) %>%
+    dplyr::add_row(day = 1)
+
+  microcycle
+}
+
+new_workout <- function(microcycle){
+  current_day = max(microcycle$day)
+  microcycle %>%
+    dplyr::add_row(day = current_day + 1)
+}
+
+add_exercise <- function(microcycle, exercise, sets, target_reps = NA_character_, target_rpe = NA_integer_, target_pct = NA_integer_){
+  microcycle %>%
+    dplyr::add_row(exercise = exercise, sets = 1:sets, target_reps = target_reps, target_rpe = target_rpe, target_pct = target_pct) %>%
+    tidyr::fill(day, .direction = "down") %>%
+    dplyr::filter(!is.na(exercise))
+}
